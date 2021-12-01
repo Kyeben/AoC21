11345d41b01e005c96c2aef6e6c53e07c0cfb86c
Kyeben <73894012+Kyeben@users.noreply.github.com>
2021-12-01 16:05:58 +0100

    Delete comments

--- day1/day12.py
+++ day1/day12.py
@@ -1,8 +1,5 @@
-
-
 if __name__ == '__main__':
-    with open('input11.txt', 'r') as reader:
-    # with open('sample.txt', 'r') as reader:
+    with open('input11.txt', 'r') as reader:    
         line = reader.read().splitlines()
         data = [int(x) for x in line]
 
@@ -12,25 +9,13 @@ if __name__ == '__main__':
         temp_increment = []
         for i in range(0, len(data)):
             if i+2 < len(data):
-                # print("{},{},{}".format(data[i], data[i+1], data[i+2]))
                 sum_depth = data[i] + data[i+1] + data[i+2]
                 sum_increments.append(sum_depth)
 
         for depth in sum_increments:
-            # print("depth: {}" .format(depth))
-            # print("prevdepth: {}".format(prev_depth))
             if prev_depth is not None:
                 if depth - prev_depth > 0:
                     increments += 1
             prev_depth = depth
-
-
-
-            # # print("depth: {}" .format(depth))
-            # # print("prevdepth: {}".format(prev_depth))
-            # if prev_depth is not None:
-            #     if depth-prev_depth > 0:
-            #         increments += 1
-            # prev_depth = depth
 
         print(increments)
